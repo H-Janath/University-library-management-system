9a62752a69102154f766a757ea395629f211a856
H-Janath <janathhma@gmail.com>
2025-03-01 13:48:05 +0530

    Add book components and configure image handling in Next.js

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,17 @@
 import BookList from '@/components/BookList'
 import BookOverview from '@/components/BookOverview'
-import { Button } from '@/components/ui/button'
+import {sampleBooks} from '@/constant'
 import React from 'react'
 
 const Home = () => {
   return (
     <>
-    <BookOverview/>
-    <BookList/>
+    <BookOverview {...sampleBooks[0]}/>
+    <BookList
+    title="Lates Books"
+    books={sampleBooks}
+    containerClassName="mt-28"
+    />
     </>
   )
 }
